5fa8ddddbda22935529d39dda7cafae28431a3ee
Thedward Blevins <thedward@barsoom.net>
2022-01-16 21:50:19 -0600

    feat: convert Google Authenticator export into list of otpauth URIs

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,18 @@
 
 module Main where
 
-import Proto.GoogleAuthMigration_Fields (secret,name,issuer,algorithm,digits,type',counter)
-import Proto.GoogleAuthMigration (MigrationPayload(..),Algorithm(..),OtpType(..),DigitCount(..))
+import System.Environment (getArgs)
+import Proto.GoogleAuthMigration_Fields (algorithm,configs,counter,digits,issuer,name,otpType,secret)
+import Proto.GoogleAuthMigration (Algorithm(..),DigitCount(..),MigrationPayload(..),OTPConfig(..),OTPType(..))
 import Data.ProtoLens (defMessage,showMessage)
 import Data.ProtoLens.Encoding (decodeMessage)
+import Data.Maybe (fromMaybe)
 import Lens.Micro
-import Text.URI (mkQueryKey,mkURI)
-import Text.URI.Lens (queryParam,uriQuery,unRText)
+import Text.URI (emptyURI,isPathAbsolute,mkHost,mkPathPiece,mkQueryKey,mkQueryKey,mkQueryValue,mkScheme,mkURI,QueryParam(..),render,URI)
+import Text.URI.Lens (queryParam,uriQuery,unRText,uriScheme,uriAuthority,authHost,uriPath)
 import Data.Text (Text(..))
 import Data.ByteString (ByteString(..))
+import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Text.Encoding as T (encodeUtf8,decodeUtf8)
@@ -25,7 +28,35 @@ getMigrationData migrationURI = do uri <- mkURI migrationURI
 decodeMigrationPayload :: ByteString -> Maybe MigrationPayload
 decodeMigrationPayload bs = either (const Nothing) return $ decodeMessage bs
 
+getConfigs bs = do payload <- decodeMigrationPayload bs
+                   let cs = payload ^. configs
+                   return cs
+
+otpURI :: OTPConfig -> Maybe URI
+otpURI otp = do sp <- QueryParam <$> mkQueryKey "secret" <*> mkQueryValue secret'
+                kp <- QueryParam <$> mkQueryKey "issuer" <*> mkQueryValue issuer'
+                ap <- QueryParam <$> mkQueryKey "algorithm" <*> mkQueryValue algorithm'
+                np <- mkPathPiece name'
+                bare <- mkURI $ T.append "otpauth://" otptype'
+                let uri = bare & uriPath .~ [np] & uriQuery .~ [sp,kp,ap]
+                return uri
+  where secret' = T.decodeUtf8 $ Base32.encode (otp ^. secret)
+        name' = otp ^. name
+        issuer' = otp ^. issuer
+        algorithm' = T.pack . show $ otp ^. algorithm
+        digits' = fromDigitCount $ otp ^. digits
+        otptype' = fromOTPType $ otp ^. otpType
+        counter' = T.pack . show $ otp ^. counter
+        fromOTPType ot = case ot of
+                              HOTP -> "hotp"
+                              _    -> "totp"
+        fromDigitCount dc = case dc of
+                                 EIGHT -> "8" 
+                                 _     -> "6"
+
+extractURIs bs = fromMaybe []  (getMigrationData bs >>= getConfigs >>= mapM (fmap render . otpURI ) )
+
 main :: IO ()
-main = undefined
-
-
+main = do args <- fmap (map T.pack) getArgs
+          let uris = concatMap extractURIs args
+          mapM_ T.putStrLn uris
